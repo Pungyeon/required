3f4eb2486fb485e2c6a48d546ccd5ccfbc31ec03
Lasse Martin Jakobsen <lmj@corti.ai>
2019-12-21 12:55:31 +0100

    added sample

---
+++ samples/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Pungyeon/json-validation/pkg/required"
+)
+
+type Customer struct {
+	ID      int32                `json:"id"`
+	Name    required.String      `json:"name"`
+	Address Address              `json:"address"`
+	Tags    required.StringSlice `json:"tags"`
+}
+
+type Address struct {
+	StreetAddress1 required.String `json:"street_address_1"`
+	StreetAddress2 string          `json:"street_address_2"`
+	Country        required.String `json:"country"`
+	PostalCode     required.Int    `json:"postal_code"`
+}
+
+func main() {
+	jsonBytes := []byte(`{
+		"name": "BigCustomer",
+		"address": {
+			"street_address_1": "Some General Name 24B",
+			"country": "Dingeling",
+			"postal_code": 91210
+		},
+		"tags": ["big boi", "customer"]
+
+	}`)
+
+	var customer Customer
+	if err := required.Unmarshal(jsonBytes, &customer); err != nil {
+		panic(err)
+	}
+	fmt.Println(customer)
+}
